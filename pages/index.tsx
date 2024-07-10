d71daa35ab02a7acdb00d9e4e576f3dbd207e4a1
Vishal Singh <vishalsingh173911@gmail.com>
2024-07-10 16:59:32 +0530

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,7 +58,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Vishal pushing this repo to firebase
+            Vishal pushing this repo to firebasee
           </p>
         </a>
 
